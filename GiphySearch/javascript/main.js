524da7c358f80269b03b16146e22bea46ca27962
Dev Sharma <dev.18715@knit.ac.in>
2020-12-15 21:22:44 +0530

    Updated

--- GiphySearch/javascript/main.js
+++ GiphySearch/javascript/main.js
@@ -1,60 +1,56 @@
-/* 1. Grab the input value */
-
-
-document.querySelector(".js-go").addEventListener('click',function(){
-
-  var input = document.querySelector("input").value;
-  pushToDOM(input);
-
+document.querySelector(".js-go").addEventListener("click", function () {
+  var inputValue = document.querySelector(".js-userinput").value;
+  var userInput = getUserInput();
+  searchGiphy(userInput);
 });
 
-document.querySelector(".js-userinput").addEventListener('keyup',function(e){
-
-  var input = document.querySelector("input").value;
-
-  // if the key ENTER is pressed...
-  if(e.which === 13) {
-    pushToDOM(input);
-  }
-
-});
-
-/* 2. do the data stuff with the API */
-
-var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
-
-// AJAX Request
-var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open( 'GET', url );
-GiphyAJAXCall.send();
-
-GiphyAJAXCall.addEventListener('load',function(e){
-
-  var data = e.target.response;
-  pushToDOM(data);
-
-});
-
-
-
-
-/* 3. Show me the GIFs */
-
-
-function pushToDOM(input) {
-
-  var response = JSON.parse(input);
-
-  var imageUrls = response.data;
-
-  imageUrls.forEach(function(image){
-
-    var src = image.images.fixed_height.url;
-    console.log(src);
-
-    var container = document.querySelector(".js-container");
-    container.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">";
-
+document
+  .querySelector(".js-userinput").addEventListener("keyup", function (data) {
+    if (data.which === 13) {
+      var userInput = getUserInput();
+      searchGiphy(userInput);
+    }
   });
 
+function getUserInput() {
+  var inputValue = document.querySelector(".js-userinput").value;
+
+  return inputValue;
+}
+
+function searchGiphy(searchQuery) {
+  var url =
+    "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + searchQuery;
+  // AJAX Request
+  var GiphyAJAXCall = new XMLHttpRequest();
+  GiphyAJAXCall.open("GET", url);
+  GiphyAJAXCall.send();
+
+  GiphyAJAXCall.addEventListener("load", function (data) {
+    var actualData = data.target.response;
+    pushToDOM(actualData);
+    console.log(actualData);
+  });
+}
+
+function pushToDOM(response) {
+  // turn response into real javascript object
+  response = JSON.parse(response);
+  // drill down to the data array
+  var images = response.data;
+
+  // find the container to hold this stuff in DOM
+  var container = document.querySelector(".js-container");
+  // clear it of old content since this function will be used on every search
+  // we want to reset the div
+  container.innerHTML = "";
+
+  // loop through data array and add IMG html
+  images.forEach(function (image) {
+    // find img src
+    var src = image.images.fixed_height.url;
+
+    // concatenate a new IMG tag
+    container.innerHTML += "<img src='" + src + "' class='container-image' />";
+  });
 }
