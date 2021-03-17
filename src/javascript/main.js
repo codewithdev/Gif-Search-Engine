79fdfa106c7de42c63eb1b4ddad7292fc9ba699f
admin-adb <adbadmin@protonmail.ch>
2021-03-17 13:00:08 +0530

    added timer

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,14 +1,13 @@
 document.querySelector(".js-go").addEventListener("click", function () {
   var inputValue = document.querySelector(".js-userinput").value;
   var userInput = getUserInput();
-  searchDocs(userInput);
+  searchDocs(userInput, new Date());
 });
 
-document
-  .querySelector(".js-userinput").addEventListener("keyup", function (data) {
+document.querySelector(".js-userinput").addEventListener("keyup", function (data) {
     if (data.which === 13) {
       var userInput = getUserInput();
-      searchDocs(userInput);
+      searchDocs(userInput, new Date());
     }
   });
 
@@ -36,13 +35,13 @@ async function postData(url = '', data = {}) {
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
-function searchDocs(searchQuery) {
+function searchDocs(searchQuery, startTime) {
   var url = "/api/search";
 
   postData(url, { query: searchQuery })
   .then(data => {
     console.log(data); // JSON data parsed by `data.json()` call
-    pushToDOM(data.result);
+    pushToDOM(data.result, startTime);
   });
 
 
@@ -59,25 +58,31 @@ function searchDocs(searchQuery) {
   // });
 }
 
-function pushToDOM(response) {
+function pushToDOM(response, startTime) {
   // turn response into real javascript object
   // response = JSON.parse(response);
   // drill down to the data array
   // var images = response.data;
 
+  // time elapsed
+  endTime = new Date();
+  var timeDiff = endTime - startTime;
+  timeDiff /= 1000;
+
   // find the container to hold this stuff in DOM
   var container = document.querySelector(".js-container");
   // clear it of old content since this function will be used on every search
   // we want to reset the div
-  container.innerHTML = "";
+  container.innerHTML = container.innerHTML += "<p class='container-timer'>Found "+Object.keys(response).length+" results in "+timeDiff+" seconds."+"</p>";
 
   // loop through data array and add IMG html
   for (const key in response) {
     // find img src
     var src = key;
 
-    // concatenate a new IMG tag
-    container.innerHTML += "<a href='" + src + "' class='container-url'>"+src+"</a>";
+    // concatenate a new url
+    container.innerHTML += "<a href='" + src + "' class='container-url' target='_blank'>"+src+"</a>";
+    container.innerHTML += "<b><p class='container-title'>Score: "+response[key]+"</p></b>";
     // container.innerHTML += "<b><p class='container-title'>Lorem Ipsum is simply dummy text</p></b>";
     // container.innerHTML += "<p class='container-description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>";
   }
